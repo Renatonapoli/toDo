c7016d299ad84fe5e621f4476b08c4cc605f63e3
Renato Napoli <renato.napoli@nagro.app>
2023-06-14 18:35:12 -0300

    FeatÇ: Inserindo / Excluindo/ Contando / Riscando

--- src/components/Lists/Lists.tsx
+++ src/components/Lists/Lists.tsx
@@ -1,32 +1,92 @@
+import { useState } from "react"
 import styles from "./Lists.module.css"
-
 import { Search } from "../Search/Search"
 
-export function Lists() {
+interface ListsProps {}
+
+export function Lists(props: ListsProps) {
+  const [tasks, setTasks] = useState<string[]>([])
+  const [completedTasks, setCompletedTasks] = useState<number>(0)
+  const [checkedTask, setCheckedTask] = useState<number | null>(null)
+  const [lastCheckedTask, setLastCheckedTask] = useState<number | null>(null)
+
+  function handleAddTask(task: string) {
+    setTasks((prevTasks) => [...prevTasks, task])
+  }
+
+  function handleDeleteTask(index: number) {
+    setTasks((prevTasks) => {
+      const newTasks = [...prevTasks]
+      newTasks.splice(index, 1)
+      return newTasks
+    })
+    setCheckedTask((prevCheckedTask) => {
+      if (prevCheckedTask === index) {
+        return null
+      }
+      return prevCheckedTask
+    })
+    setCompletedTasks((prevCompletedTasks) => prevCompletedTasks - 1)
+  }
+
+  function handleTaskStatusChange(index: number) {
+    if (checkedTask === index) {
+      setCheckedTask(null)
+      setCompletedTasks((prevCompletedTasks) => prevCompletedTasks - 1)
+    } else {
+      setCheckedTask(index)
+      setCompletedTasks((prevCompletedTasks) => prevCompletedTasks + 1)
+    }
+  }
+
+  function handleRadioClick(index: number) {
+    if (index === lastCheckedTask) {
+      setCheckedTask(null)
+      setCompletedTasks((prevCompletedTasks) => prevCompletedTasks - 1)
+      setLastCheckedTask(null)
+    }
+  }
+
   return (
     <div className={styles.containerLists}>
-      <Search />
+      <Search onAddTask={handleAddTask} />
       <header>
         <div className={styles.taskCreated}>
           <p>Tarefas criadas</p>
-          <span>5</span>
+          <span>{tasks.length}</span>
         </div>
         <div className={styles.completed}>
           <p>Concluídas</p>
-          <span>2 de 5</span>
+          <span>
+            {completedTasks} de {tasks.length}
+          </span>
         </div>
       </header>
 
-      <section>
-        <ul>
-          <input type="radio" name="" id="" />
-          <p>
-            Integer urna interdum massa libero auctor neque turpis turpis
-            semper. Duis vel sed fames integer.
-          </p>
-          <img src="../../src/assets/trash.svg" alt="" />
-        </ul>
-      </section>
+      <>
+        {tasks.map((task, index) => (
+          <section key={index}>
+            <ul>
+              <input
+                type="radio"
+                name="taskRadio"
+                id={`taskRadio${index}`}
+                checked={index === checkedTask}
+                onChange={() => handleTaskStatusChange(index)}
+                onClick={() => handleRadioClick(index)}
+              />
+              <p className={index === checkedTask ? styles.completedTask : ""}>
+                {task}
+              </p>
+              <img
+                onClick={() => handleDeleteTask(index)}
+                src="../../src/assets/trash.svg"
+                alt=""
+              />
+            </ul>
+          </section>
+        ))}
+      </>
     </div>
   )
 }
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,10 +1,26 @@
 import styles from "./Search.module.css"
+import { useState } from "react"
 
-export function Search() {
+interface SearchProps {
+  onAddTask: (task: string) => void
+}
+
+export function Search({ onAddTask }: SearchProps) {
+  const [inputValue, setInpuValue] = useState("")
+
+  function handleInsertInformation() {
+    onAddTask(inputValue)
+    setInpuValue("")
+  }
+
+  function handleInputchange(event: React.ChangeEvent<HTMLInputElement>) {
+    setInpuValue(event.target.value)
+  }
+
   return (
     <div className={styles.container}>
-      <input type="text" name="" id="" />
-      <button>
+      <input type="text" value={inputValue} onChange={handleInputchange} />
+      <button onClick={handleInsertInformation}>
         Criar <img src="../../src/assets/addList.svg" alt="" />
       </button>
     </div>
